28c860224e4dee6344b45b81adfa8f4f956bd6e9
claus147 <claus1478@gmail.com>
2014-09-20 20:39:01 -0400

    added basic UI
    very basic UI with buttons
    MainActivity.java: added initialized TextView that the data will be
    shown in

--- ISP/src/com/dp1415/ips/MainActivity.java
+++ ISP/src/com/dp1415/ips/MainActivity.java
@@ -4,13 +4,39 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
 
 public class MainActivity extends ActionBarActivity {
 
+	private TextView gpsLat;
+	private TextView gpsLon;
+	private TextView accelX;
+	private TextView accelY;
+	private TextView accelZ;
+	private TextView gyroX;
+	private TextView gyroY;
+	private TextView gyroZ;
+	private TextView magnetX;
+	private TextView magnetY;
+	private TextView magnetZ;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		gpsLat = (TextView) findViewById(R.id.gpsLat);
+		gpsLon = (TextView) findViewById(R.id.gpsLon);
+		accelX = (TextView) findViewById(R.id.accelX);
+		accelY = (TextView) findViewById(R.id.accelY);
+		accelZ = (TextView) findViewById(R.id.accelZ);
+		gyroX = (TextView) findViewById(R.id.gyroX);
+		gyroY = (TextView) findViewById(R.id.gyroY);
+		gyroZ = (TextView) findViewById(R.id.gyroZ);
+		magnetX = (TextView) findViewById(R.id.magnetX);
+		magnetY = (TextView) findViewById(R.id.magnetY);
+		magnetZ = (TextView) findViewById(R.id.magnetZ);
+
 	}
 
 	@Override
