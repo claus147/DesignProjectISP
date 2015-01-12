b0719fdeba5f7fcf098271b6de8d5031e2f44136
Rita Qin <chongxuan.qin@mail.mcgill.ca>
2015-01-11 22:13:43 -0500

    added stateVector method

--- ISP/src/com/dp1415/ips/stateVector.java
+++ ISP/src/com/dp1415/ips/stateVector.java
@@ -1,5 +1,15 @@
 package com.dp1415.ips;
+
+import java.util.HashMap;
+import java.util.Map;
+
 //this class creates a state vectors from sensor data and converts to global coordinates 
 public class stateVector {
 
+	
+	public Map<String, Float> getStateVector(){
+		Map<String, Float> stateVector = new HashMap<String, Float>(); 
+		//store the state vactors into a hashmap 
+		return stateVector;
+	}
 }
