ab6c782121d043967b211ac0efff31a49cfac64d
Rita Qin <chongxuan.qin@mail.mcgill.ca>
2015-01-20 21:30:36 -0500

    Initialize hashmap

--- ISP/src/com/dp1415/ips/stateVector.java
+++ ISP/src/com/dp1415/ips/stateVector.java
@@ -2,7 +2,7 @@ package com.dp1415.ips;
 
 import java.util.HashMap;
 import java.util.Map;
-//this class creates a state vectors from sensor data and converts to global coordinates 
+//this class creates a state vector matrix from sensor data and converts to global coordinates 
 public class stateVector {
 	// Constructor
 	public stateVector(float[] accelValues, float[] rotateValues) {
@@ -13,15 +13,37 @@ public class stateVector {
 		float rotateY = rotateValues[1];
 		float rotateZ = rotateValues[2];
 		float rotateS = rotateValues[3];
+		
+		Map<String, Float> stateVector = new HashMap<String, Float>(); 
+		//store the state vactors into a hashmap 
+		
+		//Initialize the state vector assuming initially stop 
+		stateVector.put("AccelX", new Float(0));
+		stateVector.put("AccelY", new Float(0));
+		stateVector.put("AccelZ", new Float(0));
+		stateVector.put("VelocityX", new Float(0));
+		stateVector.put("VelocityY", new Float(0));
+		stateVector.put("VelocityZ", new Float(0));
+		stateVector.put("DistanceX", new Float(0));
+		stateVector.put("DistanceY", new Float(0));
+		stateVector.put("DistanceZ", new Float(0));
+		stateVector.put("QuaX", new Float(0));
+		stateVector.put("QuaY", new Float(0));
+		stateVector.put("QuaZ", new Float(0));
+		stateVector.put("QuaS", new Float(1));
+		stateVector.put("Time", new Float(0));
+		stateVector.put("Mode", new Float(0));
 	}
 	// change phone coodinate to world coordinate
 	public float[] phoneToWorldCoordinates() {
+		
 		return null;
 	}
 	
-	public Map<String, Float> getStateVector(){
-		Map<String, Float> stateVector = new HashMap<String, Float>(); 
-		//store the state vactors into a hashmap 
-		return stateVector;
-	}
+	
+	
+	//public Map<String, Float> updateStateVector(){
+
+		//return 
+	//}
 }
