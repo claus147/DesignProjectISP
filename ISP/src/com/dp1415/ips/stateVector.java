dbff212080821134ec86d45780226427adeaa663
Ryunsky <yuanyun.chen@hotmail.com>
2015-01-22 12:13:42 -0500

    [added] phone coordinate to world coordinate method

--- ISP/src/com/dp1415/ips/stateVector.java
+++ ISP/src/com/dp1415/ips/stateVector.java
@@ -3,19 +3,20 @@ package com.dp1415.ips;
 import java.util.HashMap;
 import java.util.Map;
 import Jama.Matrix;
+import java.util.Arrays;
 
 
 //this class creates a state vector matrix from sensor data and converts to global coordinates 
 public class stateVector {
-	double accelX;
-	double accelY;
-	double accelZ;
-	double rotateX;
-	double rotateY;
-	double rotateZ;
-	double rotateS;
-	Matrix q;
-	Matrix qc;
+	private double accelX;
+	private double accelY;
+	private double accelZ;
+	private double rotateX;
+	private double rotateY;
+	private double rotateZ;
+	private double rotateS;
+	private double accelValues[];
+	private double rotateValues[];
 	
 	Matrix quaternion;
 	Map<String, Double> stateVector = new HashMap<String, Double>(); 
@@ -25,7 +26,7 @@ public class stateVector {
 
 	
 	// Constructor
-	public stateVector(float[] accelValues, float[] rotateValues) {
+	public stateVector(double[] accelValues, double[] rotateValues) {
 		accelX = accelValues[0];
 		accelY = accelValues[1];
 		accelZ = accelValues[2];
@@ -33,6 +34,10 @@ public class stateVector {
 		rotateY = rotateValues[1];
 		rotateZ = rotateValues[2];
 		rotateS = rotateValues[3];
+		this.accelValues = accelValues;
+		this.rotateValues = rotateValues;
+		
+		
 
 		//Initialize the state vector assuming initially stop 
 		stateVector.put("AccelX", 0d);
@@ -52,15 +57,13 @@ public class stateVector {
 		stateVector.put("Mode", 0d);
 	}
 	// change phone coodinate to world coordinate
-	public float[] phoneToWorldCoordinates() {
-		
-		return null;
+	public double[] phoneToWorldCoordinates() {
+		Matrix accel = new Matrix(accelValues,3);
+		Matrix worldAccel = getConversionMatrix().times(accel);
+		return worldAccel.getRowPackedCopy();
 	}
-	//this method will create matrixes.
-	private void createMatrix(){
-		q = new Matrix(new double[][]{{rotateX},{rotateY},{rotateZ},{rotateS}});
-		qc = new Matrix(new double[][]{{-rotateX},{-rotateY},{-rotateZ},{rotateS}});
-	}
+
+	
 	//this method will operate a quaternion multiplication
 	private double[] quaternionMultiplication(double[] fa, double[] sa){
 		return new double[]{
@@ -69,6 +72,21 @@ public class stateVector {
 			fa[3] * sa[2] + fa[2] * sa[3] + fa[0] * sa[1] - fa[1] * sa[0],
 			fa[3] * sa[3] - fa[0] * sa[0] - fa[1] * sa[1] - fa[2] * sa[2]
 		};
+	}
+	
+	//this method will return a 3x3 conversion matrix
+	private Matrix getConversionMatrix(){
+		double[] con_rotate = new double[]{-rotateValues[0],-rotateValues[1],-rotateValues[2],rotateValues[3],};
+		double[] x_direction = quaternionMultiplication(
+				quaternionMultiplication(rotateValues,new double[]{1,0,0,0}),con_rotate);
+		double[] y_direction = quaternionMultiplication(
+				quaternionMultiplication(rotateValues,new double[]{0,1,0,0}),con_rotate);
+		double[] z_direction = quaternionMultiplication(
+				quaternionMultiplication(rotateValues,new double[]{0,0,1,0}),con_rotate);
+		x_direction = Arrays.copyOf(x_direction, x_direction.length-1);
+		y_direction = Arrays.copyOf(y_direction, y_direction.length-1);
+		y_direction = Arrays.copyOf(z_direction, z_direction.length-1);
+		return new Matrix(new double[][]{x_direction,y_direction,z_direction});
 	}
 	
 	public final double[] getVelocities(){
